817e13dc285227f22dc3bdb367202f9091cf77a7
Natalia <natajenkins@hotmail.com>
2020-11-24 22:55:20 -0600

    Linter FIx

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
 require 'slack-ruby-bot/rspec'
+require 'webmock/rspec'
+require_relative 'support/vcr'
 require 'vcr'
 require_relative '../bin/main'
