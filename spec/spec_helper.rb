89d613adfc009fe02cb66e992d09d1f85791ef22
Natalia <natajenkins@hotmail.com>
2020-11-25 14:27:19 -0600

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,4 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
 require 'slack-ruby-bot/rspec'
 require 'webmock/rspec'
-require_relative 'support/vcr'
-require 'vcr'
 require_relative '../bin/main'
